e1df5958598c29bcb6fa8dbc9be14dda7a246cc2
kadoshita <kadoshita.inct@gmail.com>
2016-06-17 21:10:52 +0900

    add login page

--- sportscast/app.js
+++ sportscast/app.js
@@ -7,10 +7,15 @@ var bodyParser = require('body-parser');
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
+var login = require('./routes/login');
 
 var app = express();
 
-app.listen(3000);
+var port=3000;
+app.listen(port);
+
+console.log('Express Server Start');
+console.log('listen port '+port);
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -26,6 +31,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
 app.use('/users', users);
+app.use('/login',login);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
