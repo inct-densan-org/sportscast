c3476be67f8824d2c70e3fa2eb709ebbad28e695
Naoki_Fujisawa <naoki.fujisawa1@gmail.com>
2016-08-18 22:27:17 +0900

    Add "createaccount.css", "createaccount.js" and Fixed "createaccount.ejs"

---
+++ sportscast/public/javascripts/createaccount.js
@@ -0,0 +1,31 @@
+
+function console_output() {
+	var form = document.forms.form1;
+	console.log('LastName:', form.LastName.value);
+	console.log('FirstName:', form.FirstName.value);
+	console.log('mail1:', form.mail1.value);
+	console.log('mail2:', form.mail2.value);
+	console.log('Tournament:', form.Tournament.value);
+	console.log('date:', form.date.value);
+	console.log('policy:', form.policy.checked);
+	console.log('Terms:', form.Terms.checked);
+};
+
+function mail_check() {
+	$("#alert").css("visibility", "visible"); // 表示できるようにして
+	$("#alert").hide(); // 隠しておく
+	if (document.forms.form1.mail1.value !== document.forms.form1.mail2.value){ // 不一致なら
+		$("#alert").show(); // 警告表示
+	} else {
+		$("#alert").hide(); // 不要かも(^^;
+	}
+}
+
+function disableSubmit(form) {
+  var elements = form.elements;
+  for (var i = 0; i < elements.length; i++) {
+    if (elements[i].type == 'submit') {
+      elements[i].disabled = true;
+    }
+  }
+}
