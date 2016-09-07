f17437f04c9ead5a00e33d55af86bd60a5878650
kadoshita <kadoshita.inct@gmail.com>
2016-09-07 13:09:13 +0900

    account data send to server

--- sportscast/public/javascripts/createaccount.js
+++ sportscast/public/javascripts/createaccount.js
@@ -1,17 +1,3 @@
-
-function console_output() {
-	var form = document.forms.form1;
-	console.log('LastName:', form.LastName.value);
-	console.log('FirstName:', form.FirstName.value);
-	console.log('mail1:', form.mail1.value);
-	console.log('mail2:', form.mail2.value);
-	console.log('Tournament:', form.Tournament.value);
-	console.log('start:', form.start.value);
-	console.log('finish:', form.finish.value);
-	console.log('policy:', form.policy.checked);
-	console.log('Terms:', form.Terms.checked);
-};
-
 function mail_check() {
 	$("#alert").css("visibility", "visible"); // 表示できるようにして
 	$("#alert").hide(); // 隠しておく
--- sportscast/routes/createaccount.js
+++ sportscast/routes/createaccount.js
@@ -3,7 +3,11 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('createaccount', { title: 'CreateAccount' });
+	res.render('createaccount', { title: 'CreateAccount'});
 });
 
+router.post('/',function(req,res,next){
+	console.log(req.body);
+	res.send('<p>登録が完了しました。</p><br><a href=\"/\">トップページに戻る</a>');
+});
 module.exports = router;
