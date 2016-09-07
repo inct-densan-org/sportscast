14425b353b4600a170cdf990340ba2f3e6613ca2
kadoshita <kadoshita.inct@gmail.com>
2016-09-07 13:50:23 +0900

    account data send by mail

--- sportscast/routes/createaccount.js
+++ sportscast/routes/createaccount.js
@@ -1,4 +1,5 @@
 var express = require('express');
+var mail=require('../modules/mail.js')
 var router = express.Router();
 
 /* GET home page. */
@@ -8,6 +9,7 @@ router.get('/', function(req, res, next) {
 
 router.post('/',function(req,res,next){
 	console.log(req.body);
+	mail(req.body);
 	res.send('<p>登録が完了しました。</p><br><a href=\"/\">トップページに戻る</a>');
 });
 module.exports = router;
