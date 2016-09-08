76e0a4c245318bf921f6ffcb8c309b7b7751d9c6
kadoshita <kadoshita.inct@gmail.com>
2016-09-08 10:51:23 +0900

    edited createaccount.js

--- sportscast/public/javascripts/createaccount.js
+++ sportscast/public/javascripts/createaccount.js
@@ -1,9 +1,10 @@
 function mail_check() {
-	$("#alert").css("visibility", "visible"); // 表示できるようにして
-	$("#alert").hide(); // 隠しておく
-	if (document.forms.form1.mail1.value !== document.forms.form1.mail2.value){ // 不一致なら
-		$("#alert").show(); // 警告表示
+	var alert=document.getElementById('alert');
+	alert.style.visibility='visible';
+	alert.style.display='none';
+	if (document.forms.form1.mail1.value !== document.forms.form1.mail2.value){
+		alert.style.display='block';
 	} else {
-		$("#alert").hide(); // 不要かも(^^;
+		alert.style.display='none';
 	}
 }
