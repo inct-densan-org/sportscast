02eb0bec26dd1cd647fcdd9ead0147155626fe3e
kadoshita <kadoshita.inct@gmail.com>
2016-09-08 13:15:05 +0900

    edited create account page

--- sportscast/public/javascripts/createaccount.js
+++ sportscast/public/javascripts/createaccount.js
@@ -1,10 +1,20 @@
 function mail_check() {
-	var alert=document.getElementById('alert');
-	alert.style.visibility='visible';
-	alert.style.display='none';
+	var mailalert=document.getElementById('mailalert');
+	mailalert.style.visibility='visible';
+	mailalert.style.display='none';
 	if (document.forms.form1.mail1.value !== document.forms.form1.mail2.value){
-		alert.style.display='block';
+		mailalert.style.display='block';
 	} else {
-		alert.style.display='none';
+		mailalert.style.display='none';
+	}
+}
+function pass_check() {
+	var passalert=document.getElementById('passalert');
+	passalert.style.visibility='visible';
+	passalert.style.display='none';
+	if (document.forms.form1.pass1.value !== document.forms.form1.pass2.value){
+		passalert.style.display='block';
+	} else {
+		passalert.style.display='none';
 	}
 }
--- sportscast/routes/createaccount.js
+++ sportscast/routes/createaccount.js
@@ -2,6 +2,18 @@ var express = require('express');
 var mail=require('../modules/mail.js');
 var router = express.Router();
 
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://61.23.8.105:47017/sportscasttestdb',
+	function(err) {
+		if (err) {
+			console.log(err);
+		}
+		else {
+			console.log('DB connection success!');
+		}
+	}
+); 
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
 	res.render('createaccount', { title: 'CreateAccount'});
@@ -9,7 +21,7 @@ router.get('/', function(req, res, next) {
 
 router.post('/',function(req,res,next){
 	console.log(req.body);
-	mail(req.body);
+	//mail(req.body);
 	res.send('<p>登録が完了しました。</p><br><a href=\"/\">トップページに戻る</a>');
 });
 module.exports = router;
