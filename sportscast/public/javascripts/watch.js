94ac2ad29bd82ebdd4dabfadae7c43f1eb697aeb
kadoshita <kadoshita.inct@gmail.com>
2016-09-15 16:55:09 +0900

    separated reply.js

--- sportscast/public/javascripts/watch.js
+++ sportscast/public/javascripts/watch.js
@@ -257,6 +257,7 @@ function onMessage(evt) {
 	}
 	//メッセージがend_castであるときの処理
 	else if (evt.type === 'end_cast') {
+		stopRecording();
 		detachVideo(id); //映像を切断
 		stopConnection(id);
 
@@ -358,7 +359,7 @@ function createObjectURL(file) {
 //setOffer関数で呼び出される
 function prepareNewConnection(id) {
 	if (id == null) {
-		console.log('idがnullです')
+		console.log('idがnullです');
 	}
 	//STUNサーバーの設定(Googleのサーバーを使用)
 	var peercon_config = {
@@ -412,17 +413,14 @@ function prepareNewConnection(id) {
 	function onRemoteStreamAdded(event) {
 		castVideo.src = createObjectURL(event.stream);
 		remoteStream=event.stream;
-		//setTimeout(startRecording(), 5*1000);
 		keepRecording=true;
 		setTimeout(startRecording, 5*1000);
-		//debug();
 		//デバッグ用ログ出力
 		console.log('リモートストリームを追加しました。');
 	}
 
 	//リモートストリームが削除されたときに、ローカルのvideo要素から削除
 	function onRemoteStreamRemoved(event) {
-		stopRecording();
 		detachVideo(this.id);
 		//デバッグ用ログ出力
 		console.log('リモートストリームを削除しました。');
@@ -502,6 +500,7 @@ function sendRequest() {
 //ユーザの要求に応じて接続を停止
 //Hang Upボタンがクリックされたときに呼び出される
 function hangUp() {
+	stopRecording();
 	//socketサーバーにJSON形式で視聴を終了したことを伝える
 	socket.json.send({
 		type: 'exit'
@@ -523,94 +522,5 @@ function fullscreen() {
 		castVideo.mozRequestFullScreen();
 	} else {
 		castVideo.requestFullScreen();
-	}
-}
-
-startRecording=function() {//録画開始
-	if (!remoteStream) {//撮影中でない場合
-		console.warn('no stream'); //警告出して終わり
-		return;
-	}
-	else {//どちらでもない場合
-		recorder = new MediaRecorder(remoteStream);//録画開始
-	}
-	recorder.ondataavailable = function (evt) {
-		console.log('data available, recordIndex=', recordIndex, ' start playback. type=', evt.data.type);
-		var videoBlob = new Blob([evt.data], {
-			type: evt.data.type
-		});
-		blobUrl = window.URL.createObjectURL(videoBlob);
-		recordIndex++;
-		console.log('keep recording index=', recordIndex);
-	};
-
-	if (keepRecording) {//start_record_buttonを押すとkeepRecordingがtrueになる→start_record_buttonが押されたときのみ実行
-		//録画開始
-		recorder.start();
-		//ログ出力
-		console.log('start recording');
-		//timerIDにtimerの情報を代入(厳密には違うけど…)
-		timerID = setTimeout(function () {
-			//録画を停止
-			recorder.stop();
-			//timerIDにnullを代入→timerの情報が削除されるので、次回以降は実行されない(タイマー終了処理)
-			timerID = null;
-			//録画開始(次のブロック)
-			startRecording();
-			//ログ出力
-			console.log('stop and start..');
-		}, 5 * 1000);//{}で囲まれた処理を5*1000ミリ秒ごとに繰り返す
-	}
-};
-
-function stopRecording() {//録画停止
-	keepRecording = false;//フラグをOFF
-	playbutton.disabled = true;
-	if (timerID) {
-		clearTimeout(timerID);
-		timerID = null;
-	}
-	if (recorder) {
-		recorder.stop();
-		console.log('stop recording');
-	}
-}
-function playRecorded() {
-	prepareFirst();
-	playAndPrepareNext();
-}
-replayVideo.onended = function () {
-	console.log('video0 ended');
-	fileIndex++;
-};
-
-function prepareFirst() {
-	var firstURL = blobUrl;
-	replayVideo.src = firstURL;
-	replayVideo.pause();
-	console.log('playFirst index=', fileIndex, ' video0 firstURL=', firstURL);
-	replayVideo.style.visibility = 'visible';
-}
-function playAndPrepareNext() {
-	if (fileIndex >= recordIndex) {
-		console.log('end of recorded blocks');
-		return;
-	}
-	var nextUrl = blobUrl;
-	if ((fileIndex % 2) == 0) {
-		replayVideo.style.visibility = 'visible';
-		replayVideo.play();
-		console.log('start video0 URL=', replayVideo.src);
-		if (fileIndex < 1024) {
-			console.log('video1 nextURL=', nextUrl);
-		}
-	}
-	else {
-		if (fileIndex < 1024) {
-			console.log('video0 nextURL=', nextUrl);
-			replayVideo.preload = 'auto';
-			replayVideo.src = nextUrl;
-			replayVideo.play();
-		}
 	}
 }
