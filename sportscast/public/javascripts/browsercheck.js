07323e0bf3d02da1093b0afebe19546a3fde3a0a
kadoshita <kadoshita.inct@gmail.com>
2016-09-22 17:01:11 +0900

    fixed browsercheck.js

--- sportscast/public/javascripts/browsercheck.js
+++ sportscast/public/javascripts/browsercheck.js
@@ -1,14 +1,28 @@
 var userAgent = window.navigator.userAgent.toLowerCase();
 var browser='';
 
-if(userAgent.indexOf('chrome')!=-1){
-	browser='chrome';
-}
-else if(userAgent.indexOf('firefox')!=-1){
-	browser='firefox';
+if(userAgent.indexOf('android')!=-1){
+	if(userAgent.indexOf('firefox')!=-1){
+		browser='firefox';
+	}
+	else if(userAgent.indexOf('chrome')!=-1){
+		browser='chrome';
+	}
+	else{
+		alert('お使いのブラウザは対応していません');
+		browser='notsupport';
+	}
 }
 else{
-	alert('お使いのブラウザは対応していません');
-	browser='notsupport';
+	if(userAgent.indexOf('chrome')!=-1){
+		browser='chrome';
+	}
+	else if(userAgent.indexOf('firefox')!=-1){
+		browser='firefox';
+	}
+	else{
+		alert('お使いのブラウザは対応していません');
+		browser='notsupport';
+	}
 }
 console.log(browser);
