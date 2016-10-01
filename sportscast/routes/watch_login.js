e5e30a50623a81dae0544886b90f14614ceffcc8
Naoki_Fujisawa <naoki.fujisawa1@gmail.com>
2016-10-01 20:38:22 +0900

    watch_login

---
+++ sportscast/routes/watch_login.js
@@ -0,0 +1,87 @@
+var express = require('express');
+var router = express.Router();
+var dbconnection = require('../modules/dbconnection.js');
+var passport = require('passport'),
+	LocalStrategy = require('passport-local').Strategy;
+
+passport.use(new LocalStrategy(
+	function(username, password, done) {
+		dbconnection.find({
+			email: username
+		}, function(err, docs) {
+			if (err) {
+				return done(null, false, {
+					message: 'ユーザー名が間違っています'
+				});
+			}
+			if (!docs.length) {
+				return done(null, false, {
+					message: 'ユーザー名が間違っています'
+				});
+			}
+			docs.forEach(function(doc) {
+				if (err) {
+					return done(err);
+				}
+				if (!doc) {
+					return done(null, false, {
+						message: 'ユーザー名が間違っています'
+					});
+				}
+				if (doc.pass != password) {
+					return done(null, false, {
+						message: 'パスワードが間違っています'
+					});
+				}
+				if(doc.enable === false){
+					console.log(doc.enable);
+					return done(null, false, {
+						message: 'このアカウントはまだ有効化されていません。'
+					});
+				}
+				return done(null, doc);
+			});
+		});
+	}));
+
+// Configure Passport authenticated session persistence.
+passport.serializeUser(function(user, done) {
+	done(null, user.email);
+});
+
+passport.deserializeUser(function(email, done) {
+	dbconnection.find({
+		email: email
+	}, function(err, docs) {
+		docs.forEach(function(doc) {
+			if (err) {
+				return done(err);
+			}
+			done(null, doc);
+		});
+	});
+});
+
+
+router.get('/',function(req, res, next) {
+	var errorMsg = '';
+	var error = req.flash().error;
+	if (error) {
+		errorMsg = error[0];
+	}
+	res.render('watch_login', {
+		title: 'Watch Login',
+		message: errorMsg
+	});
+});
+
+router.post('/',
+	passport.authenticate('local', {
+		failureRedirect: '/login',
+		failureFlash: true
+	}),
+	function(req, res, next) {
+		res.redirect('/cast?'+req.user._id);
+	}
+);
+module.exports = router;
