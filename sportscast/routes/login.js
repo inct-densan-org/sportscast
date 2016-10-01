7bd401de31873ae4bacbeb763eb4e751162766d8
kadoshita <kadoshita.inct@gmail.com>
2016-10-01 20:34:26 +0900

    add comments

--- sportscast/routes/login.js
+++ sportscast/routes/login.js
@@ -4,6 +4,7 @@ var dbconnection = require('../modules/dbconnection.js');
 var passport = require('passport'),
 	LocalStrategy = require('passport-local').Strategy;
 
+//実際のログインの処理　メールアドレスとパスワードを渡して一致するかチェック
 passport.use(new LocalStrategy(
 	function(username, password, done) {
 		dbconnection.find({
@@ -34,7 +35,6 @@ passport.use(new LocalStrategy(
 					});
 				}
 				if(doc.enable === false){
-					console.log(doc.enable);
 					return done(null, false, {
 						message: 'このアカウントはまだ有効化されていません。'
 					});
@@ -49,6 +49,7 @@ passport.serializeUser(function(user, done) {
 	done(null, user.email);
 });
 
+//ログイン情報をセッションに保存する処理(らしい)メールアドレスをもとにユーザーデータを検索して保存
 passport.deserializeUser(function(email, done) {
 	dbconnection.find({
 		email: email
@@ -62,26 +63,29 @@ passport.deserializeUser(function(email, done) {
 	});
 });
 
-
+//このページにアクセスしたときに最初に実行される
 router.get('/',function(req, res, next) {
 	var errorMsg = '';
+	//ログイン時にエラーがあったときにreq.flash().errorにエラーメッセージが入ってる
 	var error = req.flash().error;
 	if (error) {
 		errorMsg = error[0];
 	}
+	//titleにLogin、messageにエラーメッセージ(あれば)を表示
 	res.render('login', {
 		title: 'Login',
 		message: errorMsg
 	});
 });
 
+//login.ejsのsubmitを押した時に実行される
 router.post('/',
-	passport.authenticate('local', {
-		failureRedirect: '/login',
+	passport.authenticate('local', {//ログイン処理をする
+		failureRedirect: '/login',//ログインに失敗したら再度loginを表示
 		failureFlash: true
 	}),
 	function(req, res, next) {
-		res.redirect('/cast?'+req.user._id);
+		res.redirect('/cast?'+req.user._id);//ログインに成功したら配信用ページに移動
 	}
 );
 module.exports = router;
