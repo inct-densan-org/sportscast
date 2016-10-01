3624f65a1f099879d2b100bd7b7d2fdf5c21a381
Naoki_Fujisawa <naoki.fujisawa1@gmail.com>
2016-10-01 22:54:06 +0900

    公開範囲の表示，非表示の切り替えの追加

--- sportscast/public/javascripts/createaccount.js
+++ sportscast/public/javascripts/createaccount.js
@@ -18,3 +18,33 @@ function pass_check() {
 		passalert.style.display='none';
 	}
 }
+
+function watchable_opne () {
+	document.getElementById('watchable').style.display = '';
+	document.getElementById('watchable_opne').style.display = 'none';
+	document.getElementById('watchable_close').style.display = '';
+}
+function watchable_close() {
+	document.getElementById('watchable').style.display = 'none';
+	document.getElementById('watchable_opne').style.display = '';
+	document.getElementById('watchable_close').style.display = 'none';
+	document.forms.form1.watchID00.value = '';
+	document.forms.form1.watchID01.value = '';
+	document.forms.form1.watchID02.value = '';
+	document.forms.form1.watchID03.value = '';
+	document.forms.form1.watchID04.value = '';
+	document.forms.form1.watchID05.value = '';
+	document.forms.form1.watchID06.value = '';
+	document.forms.form1.watchID07.value = '';
+	document.forms.form1.watchID08.value = '';
+	document.forms.form1.watchID09.value = '';
+	document.getElementById('watchID01').style.display = 'none';
+	document.getElementById('watchID02').style.display = 'none';
+	document.getElementById('watchID03').style.display = 'none';
+	document.getElementById('watchID04').style.display = 'none';
+	document.getElementById('watchID05').style.display = 'none';
+	document.getElementById('watchID06').style.display = 'none';
+	document.getElementById('watchID07').style.display = 'none';
+	document.getElementById('watchID08').style.display = 'none';
+	document.getElementById('watchID09').style.display = 'none';
+}
