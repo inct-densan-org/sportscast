3812dfb5fda79d5ec9fdac49ff469757e2021f1e
d14327 <g14327@g.ichinoseki.ac.jp>
2016-10-06 21:41:41 +0900

    変数名を改善しました

--- sportscast/public/javascripts/sportssearch.js
+++ sportscast/public/javascripts/sportssearch.js
@@ -1,28 +1,24 @@
 function search() {
 	var sportsnametitleelements = document.getElementsByClassName('sportsnametitle');
 	var input=document.getElementById('competition');
-	var cserch = document.getElementsByClassName('data');
-	var cnames = document.getElementsByClassName('tname');
-	var name = document.getElementById('strname');
-	var tname = document.getElementsByClassName('tname');
+	var tournamentnameelements = document.getElementsByClassName('tname');
+
 	for(var i=0;i<sportsnametitleelements.length;i++){
-		var tmp1 = cserch[i];
-		var tmp2 = sportsnametitleelements[i];
-		tmp1.parentNode.parentNode.parentNode.style.display='';
-		tmp2.parentNode.parentNode.parentNode.style.display='';
+		var sportsnametitleelement = sportsnametitleelements[i];
+		sportsnametitleelement.parentNode.parentNode.parentNode.style.display='';
 	}
 	for(var i=0;i<sportsnametitleelements.length;i++){
-		var tmp=sportsnametitleelements[i];
-		var tmp5 = tname[i];
+		var sportsnametitleelement =sportsnametitleelements[i];
+		var tournamentnameelement = tournamentnameelements[i];
 		var inputstr=input.value;
 		if(inputstr.indexOf('soccer')!=-1 || inputstr.indexOf('fencing')!=-1){
-			if(tmp.innerHTML != input.value){
-				var notdisplaynode=tmp.parentNode.parentNode.parentNode;
+			if(sportsnametitleelement.innerHTML != input.value){
+				var notdisplaynode=sportsnametitleelement.parentNode.parentNode.parentNode;
 				notdisplaynode.style.display='none';
 			}
 		}
-		else if(tmp5.innerHTML.indexOf(input.value) == -1){
-			var notdisplaynode1 = tmp5.parentNode.parentNode.parentNode;
+		else if(tournamentnameelement.innerHTML.indexOf(input.value) == -1){
+			var notdisplaynode1 = tournamentnameelement.parentNode.parentNode.parentNode;
 			notdisplaynode1.style.display = 'none';
 		}
 	}
